676a4cb4bd7f16b05ab229cd6b1040e941c2bddc
leesanghun <lsh93620@gmail.com>
2024-02-26 14:57:17 +0900

    이미지 추가

--- src/service/modaldata.js
+++ src/service/modaldata.js
@@ -1,10 +1,10 @@
 export default [
     {
         title:"2023년 포트폴리오",
-        summary:"2023 프로젝트 개인프로젝트, 팀프로젝트",
+        summary:"2023 프로젝트 개인프로젝트 및 팀프로젝트",
         describe:"HTML,CSS,jQury를 이용한 반응형 웹퍼블리싱 가이드입니다.",
         images:"/images/icon_markup_guide.jpg",//절대경로를 사용하니 이미지 오류로 상대경로 사용
-        pages:""
+        image:"/images/2023portfoilo.png"
     },
     {
         title:"COFFEE 클론코딩",
