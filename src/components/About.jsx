91c536fc7724da519443e71761e0f2ede5c2e493
leesanghun <lsh93620@gmail.com>
2024-03-11 17:03:12 +0900

    텍스트 수정

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,10 +26,10 @@ function About(){
                         <li className={styles["about-desc-list-name"]}>
                             <strong>이상훈</strong> ㅣ1993. 06. 20ㅣ마크업 개발자</li>
                         <li className={styles["about-desc-list-text"]}>
-                           <strong>"화려한 꽃보다 튼튼한 줄기가 되자." </strong>이것이 오래도록 지켜가야 한다고 생각했던 저의 신조입니다. 외적인 아름다움도 중요하지만, 내적인 튼튼함이 없다면 무의미하다고 생각합니다. 웹표준, 웹접근성에 관심이 있으며 현재에도 지속적으로 학습을 병행하면서 앞으로 나아가는 중입니다. 깃허브 블로그를 운영하며 공부한 것은 꾸준히 업로드하고 있습니다.
+                           <strong>"화려한 꽃보다 튼튼한 줄기가 되자." </strong>이것이 오래도록 지켜가야 한다고 생각했던 저의 신조입니다. 외적인 아름다움도 중요하지만, 내적인 튼튼함이 없다면 무의미하다고 생각합니다. 웹표준, 웹접근성에 관심이 있으며 현재에도 지속적으로 학습을 병행하면서 앞으로 나아가는 중입니다. 개발 관련 책을 읽거나 스터디모임에 참여하며 꾸준히 공부를 하고 있습니다. 
                         </li>
                         <li className={styles["about-desc-list-skill"]}>
-                            <span>skills</span>HTML, CSS, SCSS, media query, React, storybook, git, gulp, photoshop, illustrator
+                            <span>skills</span>HTML, CSS, SCSS, media query, React, oracle, git, photoshop, illustrator
                         </li>
                     </ul>
                     {contentsBox.map((content, index)=>(
